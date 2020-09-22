8630b8790bfe5ab8104adf2da7e950bd6281b95b
qserdel <quentin.serdel@gmail.com>
2020-09-22 18:08:40 +0200

    auto-commit

--- pwm.cpp
+++ pwm.cpp
@@ -3,7 +3,7 @@
 
 using namespace std;		// No need to keep using “std”
 
-int main()
+/*int main()
 {
 wiringPiSetup();			// Setup the library
 pinMode(21, OUTPUT);		// Configure GPIO0 as an output
@@ -18,5 +18,24 @@ digitalWrite(21, LOW);
 delay(500);
 }
 
+return 0;
+}*/
+
+unsigned int prevTime;		// Temporary variable that holds the previous millis value
+
+int main()
+{
+wiringPiSetup();			// Setup the library
+pinMode(26, PWM_OUTPUT);		// Configure GPIO1 as an output for PWM
+
+// Main program loop
+while(1)
+{
+	for(int i = 0; i < 1024; i++)
+	{
+		pwmWrite(26, i);
+		delay(1);
+	}
+}
 return 0;
 }

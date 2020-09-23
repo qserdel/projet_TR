29f95dcf6b5d20f5f163e1b1128109daac11011e
qserdel <quentin.serdel@gmail.com>
2020-09-23 21:43:08 +0200

    auto-commit

--- camOpen.cpp
+++ camOpen.cpp
@@ -1,15 +1,25 @@
 #include <iostream>
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/imgproc/imgproc.hpp"
+#include<opencv2/opencv.hpp>
 
-using namespace std;
+int main(int argc, char** argv)
+{
 
- int main( int argc, char** argv )
- {
-    VideoCapture cap(0); //capture the video from webcam
+        Mat output;
+        VideoCapture cap(CV_CAP_ANY);
 
-    if ( !cap.isOpened() )  // if not success, exit program
-    {
-         cout << "Cannot open the web cam" << endl;
-         return -1;
-    }
+
+        if( !cap.isOpened() )
+        {
+            cout << "Could not initialize capturing...\n";
+            return 0;
+        }
+
+
+        while(1){
+            cap >> output;
+
+            imshow("webcam input", output);
+            char c = (char)waitKey(10);
+            if( c == 27 ) break;
+          }
+}

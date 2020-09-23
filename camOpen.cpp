e137b1b73b78f26d9ac6491afc9f21b073e56484
qserdel <quentin.serdel@gmail.com>
2020-09-23 22:54:23 +0200

    auto-commit

--- camOpen.cpp
+++ camOpen.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 
         if( !cap.isOpened() )
         {
-            cout << "Could not initialize capturing...\n";
+            std::cout << "Could not initialize capturing...\n"<<endl;
             return 0;
         }
 

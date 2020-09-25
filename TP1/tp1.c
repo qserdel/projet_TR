6e38cafcea285bd408875779a43b7a601eff8a06
qserdel <quentin.serdel@gmail.com>
2020-09-25 18:01:13 +0200

    auto-commit

--- TP1/tp1.c
+++ TP1/tp1.c
@@ -14,47 +14,46 @@
 #include <semaphore.h>
 
 sem_t s;
-
-
+static unsigned int n_thread = 3;
 static pthread_cond_t c = PTHREAD_COND_INITIALIZER;
 static pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
 static int count = 0;
 
 void *sender(void *arg) {
     int i;
-    sem_wait(&s);
-    sem_wait(&s);
-    sem_wait(&s);
-
+    int j;
+    for(j=0;j<n_thread;j++){
+      sem_wait(&s);
+    }
     //pthread_mutex_lock(&m);
     for (i = 0; i < 10; i++) {
         count++;
         printf("%08lX: Increment count: count=%d\n", pthread_self(), count);
         if (count == 3) {
-            //pthread_mutex_unlock(&m);
+            pthread_mutex_unlock(&m);
             pthread_cond_broadcast(&c);
-            sem_post(&s);
-            sem_post(&s);
-            sem_post(&s);
-            
             printf("%08lX: Sent signal. count=%d\n", pthread_self(), count);
+            for(j=0;j<n_thread;j++){
+              sem_wait(&s);
+            }
         }
     }
     return NULL;
 }
 
 void *receiver(void *arg) {
-    sleep(1);
+    sleep(3);
     printf("%08lX: Begin waiting\n", pthread_self());
     sem_post(&s);
     pthread_cond_wait(&c, &m);
-    //sem_wait(&s);
+    pthread_mutex_unlock(&m);
     printf("%08lX: Received signal. count=%d\n", pthread_self(), count);
+    sem_post(&s);
     return NULL;
 }
 
 int main(int argc, char **argv) {
-    pthread_t id[4];
+    pthread_t id[n_thread+1];
     sem_init(&s, 0, 0);
     //pthread_mutex_lock(&m);
     pthread_create(&id[0], NULL, sender, NULL);

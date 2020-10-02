6e4bd9bc6e6ace2a1bc782a8a6d65012b599ad36
qserdel <quentin.serdel@gmail.com>
2020-10-02 19:47:05 +0200

    auto-commit

--- i2c.cpp
+++ i2c.cpp
@@ -9,7 +9,8 @@ int main()
 {
   uint8_t data [5];
   int fd = wiringPiI2CSetup(0x04);
-  wiringPiI2CWrite (fd,0x020);
+  if (wiringPiI2CWrite (fd, 0x30) < 0)
+           return (-1) ;
   while ((read (fd, data, 2) < 0)){
     delay (20) ;
   }

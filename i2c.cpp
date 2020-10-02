542d4b1b107f2322062c658d2fbd6ef32687d2c6
qserdel <quentin.serdel@gmail.com>
2020-10-02 16:10:22 +0200

    auto-commit

--- i2c.cpp
+++ i2c.cpp
@@ -5,11 +5,11 @@ using namespace std;
 
 int main()
 {
-  if(wiringPiI2CSetup(0x04)==-1){
-    cout<<"c'est de la merde"<<std::endl;
+  if(wiringPiI2CSetup(04)==-1){
+    cout<<"c'est raté"<<std::endl;
   };
   while(1){
-    cout<<wiringPiI2CRead(0x04)<<std::endl;
+    cout<<wiringPiI2CRead(04)<<std::endl;
     delay(50);
   }
   return 0;

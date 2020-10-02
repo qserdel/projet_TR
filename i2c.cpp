b8b54ddc3f06aa76ade97031bb31f01ed56788e5
qserdel <quentin.serdel@gmail.com>
2020-10-02 17:17:12 +0200

    auto-commit

--- i2c.cpp
+++ i2c.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 int main()
-{
+/*{
   uint16_t data [5];
   int fd = wiringPiI2CSetup(0x04);
   while ((read (fd, data, 2) < 0)){
@@ -16,4 +16,14 @@ int main()
     cout<<(int)data[i]<<endl;
   }
   return 0;
+}*/
+
+{
+  pinMode(2, INPUT);		// Configure GPIO06 as an output
+  while(1){
+    cout<<digitalRead(1)<<endl;
+    delay(50);
+  }
+  return 0;
+
 }

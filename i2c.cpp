1f5aecbbea08c13228359157014c5607589dd408
qserdel <quentin.serdel@gmail.com>
2020-10-02 16:19:04 +0200

    auto-commit

--- i2c.cpp
+++ i2c.cpp
@@ -1,22 +1,15 @@
 #include <wiringPi.h>
 #include <wiringPiI2C.h>
 #include <iostream>
-#include <cerrno>
-#include <stdlib>
+
 using namespace std;
 
 int main()
 {
   int fd = wiringPiI2CSetup(0x04);
-  if(fd==-1){
-    cout<<strerror(errno)<<endl;
-  };
-  //while(1){
+  while(1){
     cout<<wiringPiI2CRead(fd)<<endl;
-    if(wiringPiI2CRead(fd)==-1){
-      cout<<strerror(errno)<<endl;
-    };
     delay(50);
-  //}
+  }
   return 0;
 }

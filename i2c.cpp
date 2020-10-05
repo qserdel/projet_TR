4373dc83b4745c3cfdda17d34462e9b52bdccbf2
qserdel <quentin.serdel@gmail.com>
2020-10-05 17:50:29 +0200

    auto-commit

--- i2c.cpp
+++ i2c.cpp
@@ -7,19 +7,19 @@ using namespace std;
 
 int main()
 {
-  uint16_t data [2];
+  uint16_t data;
   int fd = wiringPiI2CSetup(0x04);
   if (wiringPiI2CWrite (fd, 0x30+1) < 0)
     return (-1) ;
-  while(1){
-    read (fd, data, 1);
-    cout<<(int)data[1]<<endl;
-    delay(20);
-    }
   /*while(1){
-    cout<<wiringPiI2CRead(fd)<<endl;
-    delay(10);
+    read (fd, data, 1);
+    cout<<(int)data<<endl;
+    delay(20);
   }*/
+  while(1){
+    cout<<wiringPiI2CRead(fd)<<endl;
+    delay(20);
+  }
   return 0;
 }
 

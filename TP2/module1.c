fd6c51af98a8835ce691e69013155ac1ea88a668
qserdel <quentin.serdel@gmail.com>
2020-10-08 15:56:25 +0200

    auto-commit

--- TP2/module1.c
+++ TP2/module1.c
@@ -6,10 +6,11 @@
 #include <linux/init.h>		/* module_{init,exit}() */
 #include <linux/moduleparam.h> /* parameters of module */
 
+static int param 9;
+
 static int __init modules1_init(void)
 {
-	printk("Hello World!");
-	//dmesg();
+	printk("parm = %d",parm);
 	return 0;
 }
 
@@ -22,7 +23,9 @@ static void __exit modules1_exit(void)
  */
 module_init(modules1_init);
 module_exit(modules1_exit);
+module_param(param, int, 0644);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Quentin Serdel");
 MODULE_DESCRIPTION("Driver TP");
+MODULE_PARM_DESC("param=9");

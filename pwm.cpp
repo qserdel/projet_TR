8740d8e724617718d0b1ca41a08023c7a1ee7970
qserdel <quentin.serdel@gmail.com>
2020-10-09 16:11:24 +0200

    auto-commit

--- pwm.cpp
+++ pwm.cpp
@@ -7,8 +7,8 @@ static int pin_m2 = 25; //pin gpio26
 int cam_motors_setup(){
 	if(wiringPiSetup()==-1) // Setup the library
 		return -1;
-	softPwmCreate(pin_m1,0,200); //initialise  pin m1 en pwm
-	softPwmCreate(pin_m2,0,200); //initialise pin m2 en pwm
+	softPwmCreate(pin_m1,0,100); //initialise  pin m1 en pwm
+	softPwmCreate(pin_m2,0,100); //initialise pin m2 en pwm
 }
 
 //pour faire bouger les servo d'avant en arriere
@@ -18,13 +18,13 @@ cam_motors_setup();
 
 while(1)
 {
-	for(int i = 0; i < 200; i++)
+	for(int i = 0; i < 100; i++)
 	{
 		softPwmWrite(5, i);
 		softPwmWrite(25, i);
 		delay(10);
 	}
-	for(int i = 200; i > 0; i--)
+	for(int i = 100; i > 0; i--)
   {
     softPwmWrite(5, i);
 		softPwmWrite(25, i);

2c66b4a7df89e90081130d604a782fc9e05f3ae8
qserdel <quentin.serdel@gmail.com>
2020-10-13 14:44:06 +0200

    auto-commit

--- TP2/main_char.c
+++ TP2/main_char.c
@@ -2,13 +2,16 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <sys/ioctl.h>
+#include <errno.h>
+#include <string.h>
 
 static int fd;
 
 int main(){
+  errno=0;
   fd=open("~/../../dev/mychar",'r');
   if(fd==-1)
-    printf("error in fopen");
+    printf(strerror(errno));
   ioctl(fd,IOC_OUT);
   return 0;
 }

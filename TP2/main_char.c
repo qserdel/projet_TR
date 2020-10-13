f5e8c622d3b047e5661b1d7f878452810e7a5e9c
qserdel <quentin.serdel@gmail.com>
2020-10-13 15:47:05 +0200

    auto-commit

--- TP2/main_char.c
+++ TP2/main_char.c
@@ -19,13 +19,12 @@ int main(){
   fd=open("/dev/mychar",'w');
   if(fd==-1)
     printf(strerror(errno));
-  printf(write(fd,"Hello World!",12);
-  printf("\n");
+  printf("%d\n",write(fd,"Hello World!",12);
   close(fd);
   /fd=open("/dev/mychar",'r');
   if(fd==-1)
     printf(strerror(errno));
-  printf(read(fd,buf,12);
+  printf("%d\n",read(fd,buf,12);
   close(fd);
   printf(buf);
   return 0;

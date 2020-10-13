23e6ba7819650ef12c9f3ec13b1f28c4765e2e9d
qserdel <quentin.serdel@gmail.com>
2020-10-13 17:53:18 +0200

    auto-commit

--- TP2/reader.c
+++ TP2/reader.c
@@ -10,7 +10,7 @@ char buf[50];
 
 int main(){
   errno=0;
-  if((fd=open("/dev/mychar",O_RONLY))==-1)
+  if((fd=open("/dev/mychar",O_RDONLY))==-1)
     printf("open : %s\n",strerror(errno));
   if(read(fd,buf,12)==-1)
     printf("read : %s\n",strerror(errno));
--- TP2/writer.c
+++ TP2/writer.c
@@ -10,7 +10,7 @@ static int fd;
 int main(){
 
   errno=0;
-  if((fd=open("/dev/mychar",O_WONLY))==-1)
+  if((fd=open("/dev/mychar",O_WRONLY))==-1)
     printf("open : %s\n",strerror(errno));
   if(write(fd,"Hello World!",12)==-1)
     printf("write : %s\n",strerror(errno));

3fc034a41df3ade027bc17c561c1d0c4ae47e209
qserdel <quentin.serdel@gmail.com>
2020-10-13 16:26:49 +0200

    auto-commit

--- TP2/main_char.c
+++ TP2/main_char.c
@@ -12,17 +12,17 @@ char* buf1="Hello World!";
 
 int main(){
   errno=0;
-  if((fd=open("/dev/mychar",'r'))==-1)
+  if((fd=open("/dev/mychar",O_RDWR))==-1)
     printf("open : %s\n",strerror(errno));
   if(ioctl(fd,IOC_OUT)==-1)
     printf("ioctl : %s\n",strerror(errno));
   close(fd);
-  if((fd=open("/dev/mychar",'r'))==-1)
+  if((fd=open("/dev/mychar",O_RDWR))==-1)
     printf("open : %s\n",strerror(errno));
   if(write(fd,buf1,12)==-1)
     printf("write : %s\n",strerror(errno));
   close(fd);
-  if((fd=open("/dev/mychar",'r'))==-1)
+  if((fd=open("/dev/mychar",O_RDWR))==-1)
     printf("open : %s\n",strerror(errno));
   if(read(fd,buf,12)==-1)
     printf("read : %s\n",strerror(errno));

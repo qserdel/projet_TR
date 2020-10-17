274d4d1478a7d0dc9cfec5ef17d687e66870a861
qserdel <quentin.serdel@gmail.com>
2020-10-17 22:10:24 +0200

    auto-commit

--- TP2/char.c
+++ TP2/char.c
@@ -28,7 +28,7 @@ static int flag = 0;
 static int flag0 = 0;
 static int flag1 = 0;
 static int current_flag;
-static int wq;
+static struct wait_queue_head wq;
 static DECLARE_WAIT_QUEUE_HEAD(wq0);
 static DECLARE_WAIT_QUEUE_HEAD(wq1);
 

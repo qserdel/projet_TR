09ad4c005d87ce92651c1d2a513325b770bb46d4
qserdel <quentin.serdel@gmail.com>
2020-10-17 19:14:58 +0200

    auto-commit

--- TP2/writer.c
+++ TP2/writer.c
@@ -6,13 +6,15 @@
 #include <unistd.h>
 
 static int fd;
+char*buf;
 
-int main(){
+int main(int argc, char* argv[]){
 
   errno=0;
+  buf=malloc(sizeof(argv[0]));
   if((fd=open("/dev/mychar",O_WRONLY))==-1)
     printf("open : %s\n",strerror(errno));
-  if(write(fd,"Hello World!",12)==-1)
+  if(write(fd,buf,sizeof(buf))==-1)
     printf("write : %s\n",strerror(errno));
   close(fd);
   return 0;

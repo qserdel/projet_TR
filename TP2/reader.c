e5e1321e0372a3ab66cf6d0e94be176232cb830f
qserdel <quentin.serdel@gmail.com>
2020-10-17 19:09:36 +0200

    auto-commit

--- TP2/reader.c
+++ TP2/reader.c
@@ -12,7 +12,7 @@ int main(){
   errno=0;
   if((fd=open("/dev/mychar",O_RDONLY))==-1)
     printf("open : %s\n",strerror(errno));
-  if(read(fd,buf,12)==-1)
+  if(read(fd,buf,15)==-1)
     printf("read : %s\n",strerror(errno));
   close(fd);
   printf("string recieved : %s\n",buf);

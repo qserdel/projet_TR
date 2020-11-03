07c6406f9d5ccbb29961a0afc7737027beba6be8
qserdel <quentin.serdel@gmail.com>
2020-11-03 18:58:21 +0100

    auto-commit

--- TP3/rosws/src/Controller/src/Controller-component.cpp
+++ TP3/rosws/src/Controller/src/Controller-component.cpp
@@ -2,46 +2,74 @@
 #include <rtt/Component.hpp>
 #include <iostream>
 
-Controller::Controller(std::string const& name) : TaskContext(name){
-  RTT::log(RTT::Info) << "Controller constructed !" <<RTT::endlog();
-  this->ports()->addPort("mesure",mesure);
-  this->ports()->addPort("cmd",cmd);
-  this->addAttribute("des",des);
+namespace Control {
+  Controller::Controller(std::string const& name) : TaskContext(name){
+    RTT::log(RTT::Info) << "Controller constructed !" <<RTT::endlog();
+    this->ports()->addPort("mesure",mesure).doc("mesure en entrée du controleur");
+    this->ports()->addPort("cmd",cmd).doc("commande en sortie du controleur");
+    this->ports()->addEventPort("event",event);
+    this->addAttribute("des",des);
+  }
+
+  bool Controller::configureHook(){
+    RTT::log(RTT::Info) << "Controller configured !" <<RTT::endlog();
+    return true;
+  }
+
+  bool Controller::startHook(){
+    RTT::log(RTT::Info) << "Controller started !" <<RTT::endlog();
+    return true;
+  }
+
+  void Controller::updateHook(){
+    RTT::log(RTT::Info) << "Controller executes updateHook !" <<RTT::endlog();
+    if(mesure<des){
+      cmd.write(mesure+1);
+    }
+    RTT::log(RTT::Info) << "Command = %d !",cmd <<RTT::endlog();
+  }
+
+  void Controller::stopHook() {
+    RTT::log(RTT::Info) << "Controller executes stopping !" <<RTT::endlog();
+  }
+
+  void Controller::cleanupHook() {
+    RTT::log(RTT::Info) << "Controller cleaning up !" <<RTT::endlog();
+  }
+
+  Motor::Motor(std::string const& name) : TaskContext(name){
+    RTT::log(RTT::Info) << "Motor constructed !" <<RTT::endlog();
+    this->ports()->addPort("in",in).doc("entrée du système");
+    this->ports()->addPort("out",out).doc("sortie du système");
+    this->ports()->addEventPort("event",event);
+  }
+
+  bool Motor::configureHook(){
+    RTT::log(RTT::Info) << "Motor configured !" <<RTT::endlog();
+    return true;
+  }
+
+  bool Motor::startHook(){
+    RTT::log(RTT::Info) << "Motor started !" <<RTT::endlog();
+    return true;
+  }
+
+  void Motor::updateHook(){
+    RTT::log(RTT::Info) << "Motor executes updateHook !" <<RTT::endlog();
+    double* var;
+    in.read(var);
+    out.write(var);
+  }
+
+  void Motor::stopHook() {
+    RTT::log(RTT::Info) << "Motor executes stopping !" <<RTT::endlog();
+  }
+
+  void Motor::cleanupHook() {
+    RTT::log(RTT::Info) << "Motor cleaning up !" <<RTT::endlog();
+  }
 }
 
-bool Controller::configureHook(){
-  RTT::log(RTT::Info) << "Controller configured !" <<RTT::endlog();
-  return true;
-}
-
-bool Controller::startHook(){
-  RTT::log(RTT::Info) << "Controller started !" <<RTT::endlog();
-  return true;
-}
-
-void Controller::updateHook(){
-  RTT::log(RTT::Info) << "Controller executes updateHook !" <<RTT::endlog();
-}
-
-void Controller::stopHook() {
-  RTT::log(RTT::Info) << "Controller executes stopping !" <<RTT::endlog();
-}
-
-void Controller::cleanupHook() {
-  RTT::log(RTT::Info) << "Controller cleaning up !" <<RTT::endlog();
-}
-
-/*
- * Using this macro, only one component may live
- * in one library *and* you may *not* link this library
- * with another component library. Use
- * ORO_CREATE_COMPONENT_TYPE()
- * ORO_LIST_COMPONENT_TYPE(Controller)
- * In case you want to link with another library that
- * already contains components.
- *
- * If you have put your component class
- * in a namespace, don't forget to add it here too:
- */
 ORO_CREATE_COMPONENT_LIBRARY()
-ORO_LIST_COMPONENT_TYPE(Controller)
+ORO_LIST_COMPONENT_TYPE(Control::Controller)
+ORO_LIST_COMPONENT_TYPE(Control::Motor)
--- TP3/rosws/src/Controller/src/Controller-component.hpp
+++ TP3/rosws/src/Controller/src/Controller-component.hpp
@@ -4,16 +4,33 @@
 #include <rtt/RTT.hpp>
 #include <rtt/Port.hpp>
 
-class Controller : public RTT::TaskContext{
-  public:
-    Controller(std::string const& name);
-    bool configureHook();
-    bool startHook();
-    void updateHook();
-    void stopHook();
-    void cleanupHook();
-    RTT::InputPort<double> mesure;
-    RTT::OutputPort<double> cmd;
-    double des;
-};
+namespace Control {
+  class Controller : public RTT::TaskContext{
+    public:
+      Controller(std::string const& name);
+      bool configureHook();
+      bool startHook();
+      void updateHook();
+      void stopHook();
+      void cleanupHook();
+      RTT::InputPort<double> mesure;
+      RTT::OutputPort<double> cmd;
+      RTT::InputPort<double> event;
+      double des;
+    };
+
+  class Motor : public RTT::TaskContext{
+    public:
+      Motor(std::string const& name);
+      bool configureHook();
+      bool startHook();
+      void updateHook();
+      void stopHook();
+      void cleanupHook();
+      RTT::InputPort<double> in;
+      RTT::OutputPort<double> out;
+      RTT::InputPort<double> event;
+  };
+}
+
 #endif
